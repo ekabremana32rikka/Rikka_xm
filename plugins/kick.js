07a1d8d145339a0694d4876ed7866ab8da3494d6
ekabremana32rikka <77883312+ekabremana32rikka@users.noreply.github.com>
2021-01-30 12:41:07 +0700

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -11,7 +11,7 @@ handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = true
-handler.private = false
+handler.private = true
 
 handler.admin = true
 handler.botAdmin = true
